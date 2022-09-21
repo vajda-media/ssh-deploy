a803268e51527118319357b953212ac2257f0d6e
Lorant Vajda <lortschi@gmail.com>
2022-09-21 16:33:41 +0200

    added inputs

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 #!/usr/bin/env node
-const nodeRsync = require('rsyncwrapper');
+const nodeRsync = require("rsyncwrapper");
 
-const { validateRsync, validateInputs } = require('./rsyncCli');
-const { addSshKey } = require('./sshKey');
+const { validateRsync, validateInputs } = require("./rsyncCli");
+const { addSshKey } = require("./sshKey");
 
 const {
-  REMOTE_HOST, REMOTE_USER,
-  REMOTE_PORT, SSH_PRIVATE_KEY, DEPLOY_KEY_NAME,
-  SOURCE, TARGET, ARGS, EXCLUDE,
-  GITHUB_WORKSPACE
-} = require('./inputs');
+  REMOTE_HOST,
+  REMOTE_USER,
+  REMOTE_PORT,
+  SSH_PRIVATE_KEY,
+  DEPLOY_KEY_NAME,
+  SOURCE,
+  TARGET,
+  ARGS,
+  EXCLUDE,
+  GITHUB_WORKSPACE,
+  FROM_ENV,
+  TO_ENV,
+} = require("./inputs");
 
 const defaultOptions = {
   ssh: true,
-  sshCmdArgs: ['-o StrictHostKeyChecking=no'],
-  recursive: true
+  sshCmdArgs: ["-o StrictHostKeyChecking=no"],
+  recursive: true,
 };
 
-console.log('[general] GITHUB_WORKSPACE: ', GITHUB_WORKSPACE);
+console.log("[general] GITHUB_WORKSPACE: ", GITHUB_WORKSPACE);
 
 const sshDeploy = (() => {
   const rsync = ({ privateKey, port, src, dest, args, exclude }) => {
@@ -26,51 +34,74 @@ const sshDeploy = (() => {
 
     try {
       // RSYNC COMMAND
-      nodeRsync({
-        src, dest, args, privateKey, port, excludeFirst: exclude, ...defaultOptions
-      }, (error, stdout, stderr, cmd) => {
-        if (error) {
-          console.error('⚠️ [Rsync] error: ', error.message);
-          console.log('⚠️ [Rsync] stderr: ', stderr);
-          console.log('⚠️ [Rsync] stdout: ', stdout);
-          console.log('⚠️ [Rsync] cmd: ', cmd);
-          process.abort();
-        } else {
-          console.log('✅ [Rsync] finished.', stdout);
+      nodeRsync(
+        {
+          src,
+          dest,
+          args,
+          privateKey,
+          port,
+          excludeFirst: exclude,
+          ...defaultOptions,
+        },
+        (error, stdout, stderr, cmd) => {
+          if (error) {
+            console.error("⚠️ [Rsync] error: ", error.message);
+            console.log("⚠️ [Rsync] stderr: ", stderr);
+            console.log("⚠️ [Rsync] stdout: ", stdout);
+            console.log("⚠️ [Rsync] cmd: ", cmd);
+            process.abort();
+          } else {
+            console.log("✅ [Rsync] finished.", stdout);
+          }
         }
-      });
+      );
     } catch (err) {
-      console.error('⚠️ [Rsync] command error: ', err.message, err.stack);
+      console.error("⚠️ [Rsync] command error: ", err.message, err.stack);
       process.abort();
     }
   };
 
-  const init = ({ src, dest, args, host = 'localhost', port, username, privateKeyContent, exclude = [] }) => {
+  const init = ({
+    src,
+    dest,
+    args,
+    host = "localhost",
+    port,
+    username,
+    privateKeyContent,
+    exclude = [],
+    fromEnv,
+    toEnv,
+  }) => {
     validateRsync(() => {
-      const privateKey = addSshKey(privateKeyContent, DEPLOY_KEY_NAME || 'deploy_key');
+      const privateKey = addSshKey(privateKeyContent, DEPLOY_KEY_NAME || "deploy_key");
       const remoteDest = `${username}@${host}:${dest}`;
 
       rsync({ privateKey, port, src, dest: remoteDest, args, exclude });
+      copyFile(fromEnv, toEnv);
     });
   };
 
   return {
-    init
+    init,
   };
 })();
 
 const run = () => {
-  validateInputs({ SSH_PRIVATE_KEY, REMOTE_HOST, REMOTE_USER });
+  validateInputs({ SSH_PRIVATE_KEY, REMOTE_HOST, REMOTE_USER, SCRIPT });
 
   sshDeploy.init({
-    src: `${GITHUB_WORKSPACE}/${SOURCE || ''}`,
+    src: `${GITHUB_WORKSPACE}/${SOURCE || ""}`,
     dest: TARGET || `/home/${REMOTE_USER}/`,
-    args: ARGS ? [ARGS] : ['-rltgoDzvO'],
+    args: ARGS ? [ARGS] : ["-rltgoDzvO"],
     host: REMOTE_HOST,
-    port: REMOTE_PORT || '22',
+    port: REMOTE_PORT || "22",
     username: REMOTE_USER,
     privateKeyContent: SSH_PRIVATE_KEY,
-    exclude: (EXCLUDE || '').split(',').map((item) => item.trim()) // split by comma and trim whitespace
+    exclude: (EXCLUDE || "").split(",").map((item) => item.trim()), // split by comma and trim whitespace
+    fromEnv: FROM_ENV,
+    toEnv: TO_ENV,
   });
 };
 
--- src/inputs.js
+++ src/inputs.js
@@ -1,7 +1,19 @@
-const inputNames = ['REMOTE_HOST', 'REMOTE_USER', 'REMOTE_PORT', 'SSH_PRIVATE_KEY', 'DEPLOY_KEY_NAME', 'SOURCE', 'TARGET', 'ARGS', 'EXCLUDE'];
+const inputNames = [
+  "REMOTE_HOST",
+  "REMOTE_USER",
+  "REMOTE_PORT",
+  "SSH_PRIVATE_KEY",
+  "DEPLOY_KEY_NAME",
+  "SOURCE",
+  "TARGET",
+  "ARGS",
+  "EXCLUDE",
+  "FROM_ENV",
+  "TO_ENV",
+];
 
 const inputs = {
-  GITHUB_WORKSPACE: process.env.GITHUB_WORKSPACE
+  GITHUB_WORKSPACE: process.env.GITHUB_WORKSPACE,
 };
 // Get inputs from ENV or WITH workflow settings
 inputNames.forEach((input) => {
